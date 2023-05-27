2d11c98949e985d79d6a1d0d2c346611fc831680
Francisco Santos <57752852+qtoino@users.noreply.github.com>
2023-05-27 06:47:50 +0200

    200 sounds random

--- backend/separate.py
+++ backend/separate.py
@@ -1,29 +1,11 @@
 import csv
 from math import sqrt
 import random
-import pandas as pd
 
 def distance_to_origin(x, y, z):
     return sqrt(x ** 2 + y ** 2 + z ** 2)
 
 def separate_csv_rows(input_file, output_file1, output_file2, threshold):
-
-    # Read the CSV file into a DataFrame
-    df = pd.read_csv(input_file)
-
-    # Get the first row and the remaining rows
-    first_row = df.iloc[[0]]
-    remaining_rows = df.iloc[1:]
-
-    # Randomly sample the remaining rows
-    randomized_rows = remaining_rows.sample(frac=1)
-
-    # Concatenate the first row with the randomized rows
-    randomized_df = pd.concat([first_row, randomized_rows])
-
-    # Write the randomized DataFrame back to the CSV file
-    randomized_df.to_csv(input_file, index=False)
-
     with open(input_file, newline='') as csvfile:
         reader = list(csv.reader(csvfile, delimiter=','))  # Convert reader to a list
         header = reader[0]  # Store the header row
